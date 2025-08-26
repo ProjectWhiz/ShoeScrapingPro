fc6a3ce4ab54ae41b8e0fe10d54b38eb9378eaf9
Matthew Whisonant <matthewwhisonant@protonmail.com>
2025-08-25 20:02:01 -0400

    Changing goals to align with ShoeScraperPro

--- scraper.py
+++ scraper.py
@@ -1,11 +1,28 @@
 import requests
+import gzip
+import io
 from bs4 import BeautifulSoup
 
+print("Script started")
 
-Url = "https://www.apartments.com/sitemap_AllCitySearches_Price.xml.gz"
-requests.get(Url)
+Url = "https://www.footlocker.com/en/category/mens/shoes.html"
 
 
+response = requests.get(Url)
+print(response)
 
+"""
+if requests.get(Url).status_code == 200:
+    print("Successfully connected to the first URL")
+    with gzip.GzipFile(fileobj=io.BytesIO(response.content)) as gz:
+        xml_content = gz.read().decode('utf-8')
+    print(xml_content[:500])
+#else:
+    print(f"Failed to connect. Status code: {response.status_code}")
+    
+"""
    
    
+   
+   
+
